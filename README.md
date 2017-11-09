6002d52215d2d8e11c329770010e51f31e1044cf
xxh <xxh332211@qq.com>
2017-11-10 00:12:33 +0800

    hahha

--- README.md
+++ README.md
@@ -17,5 +17,15 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
+# flexble
+项目采用了vue的flexble插件，会将px单位转换成rem，
+github：  https://github.com/Jinjiang/px2rem-loader；
+中文教程： http://blog.csdn.net/z1712636234/article/details/77881685；
+ps：目前设置是默认的以750px设计稿为准；
+# element ui
+elementUi 是饿了么研发并使用的一套ui库，可以和react，vue等框架共同使用 
+中文官网：http://element-cn.eleme.io/#/zh-CN
+github： https://github.com/ElemeFE/element
+
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,20 +1,53 @@
 <template>
   <div class="home">
     <el-row class="home_container">
-      <el-col class="nav" :span="4">
+      <div class="nav">
         <div class="logo">
           <img :src="pic.logopic" alt="大袜网OMS">
         </div>
         <div class="nav_content">
           <!-- 这是nav的内容 -->
+          <el-row class="tac">
+            <el-col :span="24">
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-menu"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+              </el-menu>
+            </el-col>
+          </el-row>
         </div>
-      </el-col>
-      <el-col class="content" :span="20">
-        <div class="header_nav"></div>
+      </div>
+      <div class="content">
+        <div class="header_nav">
+          
+        </div>
         <div class="main_content">
           <router-view></router-view>
         </div>
-      </el-col>
+      </div>
     </el-row>
   </div>
 </template>
@@ -31,6 +64,14 @@ export default{
         logopic: Logopic
       }
     }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
@@ -38,38 +79,47 @@ export default{
 <style rel="stylesheet/scss" scoped lang="scss">
 .home {
   height:100%;
+  margin:0 auto;
+  max-width: 1920px;
   .home_container {
     height:100%;
     .nav {
       height: 100%;
-      position: relative;
+      width:200px;
+      float: left;
+      position:relative;
+      padding-top:60px;
+      .el-menu-vertical-demo {
+        border: none;
+      }
       .logo {
-        background: black;
-        height:100px;
+        background: #fff;
         overflow: hidden;
+        width: 200px;
+        height: 60px;
         position: absolute;
         top:0;
-        left: 0;
-        width: 100%;
+        left:0;
         img {
-          width: 100%;
+          width: 200px;
         }
       }
       .nav_content {
-        background: pink;
+        background: #545c64;
         height: 100%;
-        padding-top:100px;
       }
     }
     .content {
       height: 100%;
+      width:100%;
+      padding-left: 200px;
+      background: #f5f5f5;
       .header_nav {
-        height: 100px;
-        background: black;
+        height: 60px;
+        background: #fff;
       }
     }
   }
-  
 }
 </style>
 
